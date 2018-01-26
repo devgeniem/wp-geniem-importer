f157b384fe9dcc8ec16d0e7d68c1fda6dab40774
Timi-Artturi Mäkelä <timi.makela@geniem.com>
2018-01-26 12:28:34 +0200

    Version bump 0.2.5

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Geniem Importer
  * Plugin URI:  https://github.com/devgeniem/geniem-importer
  * Description: An object-oriented and developer friendly WordPress importer.
- * Version:     0.2.4
+ * Version:     0.2.5
  * Author:      Geniem
  * Author URI:  http://www.geniem.fi/
  * License:     GPL3
@@ -33,7 +33,7 @@ class Importer_Plugin {
      * @var array
      */
     protected static $plugin_data = [
-        'VERSION' => '0.2.3',
+        'VERSION' => '0.2.5',
     ];
 
     /**
